0e6dccada054a086d4f5cf9f07e3b867196817fc
2L <lucas.littner@gmail.com>
2022-06-11 11:35:04 +0200

    ajout fonction date et note

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Cards = ({ movie }) => {
+  const date = new Date(movie.release_date);
+  const newDate = date.toLocaleDateString("fr-FR", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
   return (
     <div className="card-container">
       <div className="card">
@@ -10,10 +17,13 @@ const Cards = ({ movie }) => {
           alt={"affiche du film :" + movie.title}
         />
 
-        <p>Sortie le : {movie.release_date}</p>
+        {movie.release_date ? (
+          <p> sortie le : {newDate}</p>
+        ) : (
+          <p>Date de sortie inconnue</p>
+        )}
         <div className="vote-container">
-          <span>{movie.vote_average}</span>
-          <span>{movie.vote_count}</span>
+          <span>{movie.vote_average}/10 &#9733;</span>
         </div>
       </div>
     </div>
